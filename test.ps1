5a1483b11fae5488b9da29572cf31e4ee6f79afa
Davide Bedin <dabedin@microsoft.com>
2020-06-24 16:38:54 +0200

    cleanup

--- test.ps1
+++
@@ -1,2 +0,0 @@
-# https://docs.locust.io/en/stable/running-locust-docker.html#running-locust-with-docker
-docker run -p 8089:8089 -v c:/dockershare:/mnt/locust locustio/locust:1.0.3 -f /mnt/locust/locustfile.py
